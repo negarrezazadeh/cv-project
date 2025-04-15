9ed5be7e4fd490eb3753dbae78cf779daa982096
rznegrr <negarrz1378@gmail.com>
2025-04-15 23:24:17 +0330

    implementing fa languages

--- src/component/layout.js
+++ src/component/layout.js
@@ -5,10 +5,12 @@ import Work from "./Work/work";
 import ContactMe from "./contact me/contact-me";
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import { useTranslation } from "react-i18next";
 
 export default function Layout(props) {
+  const {i18n} = useTranslation()
   return (
-    <div>
+    <div dir={i18n.language === "fa" ? "rtl" : "ltr"}>
       <Header onClick={props.onClick}/>
       <Hero />
       <Skills />
