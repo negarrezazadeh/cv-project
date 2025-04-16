bd57f1e241162701b519f67a069e0e23aa1b2c87
rznegrr <negarrz1378@gmail.com>
2025-04-16 12:26:03 +0330

    add theme toggle button

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors:{
-      'default' : '#ffffff',
-      'gray50' : '#f9fafb',
-      'gray100' : '#f3f4f6',
-      'gray200' : '#e5e7eb',
-      'gray400' : '#9ca3af',
-      'gray600' : '#4b5563',
-      'gray700' : '#374151',
-      'gray900' : '#111827',
-      'darkgray-d' : '#030712',
-      'darkgray50' : '#111827',
-      'darkgray600' : '#d1d5db',
-      'darkgray900' : '#f9fafb'
+    extend: {
+      colors: {
+        default: '#ffffff',
+        gray50: '#f9fafb',
+        gray100: '#f3f4f6',
+        gray200: '#e5e7eb',
+        gray400: '#9ca3af',
+        gray600: '#4b5563',
+        gray700: '#374151',
+        gray900: '#111827',
+        'darkgray-d': '#030712',
+        darkgray50: '#111827',
+        darkgray600: '#d1d5db',
+        darkgray900: '#f9fafb',
+      },
     },
   },
   plugins: [],
 }
-
